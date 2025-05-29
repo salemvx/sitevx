151413ba51aa97cec0cd544c9e11c6a01ff28421
Salemvx <salemlucas7@gmail.com>
2025-05-29 13:23:29 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/apps")
+def apps():
+    return render_template("apps.html")
+
+@app.route("/ia")
+def ia():
+    return render_template("ia.html")
+
+@app.route("/videos")
+def videos():
+    return render_template("videos.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
