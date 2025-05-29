5fcc8c15cab4923019ce0e5045787993400689e9
Salemvx <salemlucas7@gmail.com>
2025-05-29 14:23:56 -0300

    app.py

--- app.py
+++ app.py
@@ -19,6 +19,14 @@ def ia():
 def videos():
     return render_template("videos.html")
 
+@app.route('/politica-de-privacidade')
+def privacy():
+    return render_template('privacy.html')
+
+@app.route('/termos-de-uso')
+def termos():
+    return render_template('terms.html')
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
